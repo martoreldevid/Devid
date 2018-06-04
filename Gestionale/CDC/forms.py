c858fdb3b23af1527981203a510a68dbdc3753a5
martoreldevid <martoreldevid@yahoo.it>
2018-06-04 06:57:06 +0000

    quasiFinitiForm

--- Gestionale/CDC/forms.py
+++ Gestionale/CDC/forms.py
@@ -14,3 +14,44 @@ class addCCIAAForm(forms.Form):
 	sede = forms.CharField(label='Sede', max_length=45)
 	descrizione = forms.CharField(label='Descrizione', max_length=45)
 
+
+class addDipendenteForm(forms.Form):
+	cf = forms.CharField(label='CF', max_length=30)
+	cognome = forms.CharField(label='Cognome', max_length=45)
+	nome = forms.CharField(label='Nome', max_length=45)
+
+class addRestituzioneForm(forms.Form):
+	tipo  = forms.CharField(label='Tipo', max_length=45)
+	descrizione = forms.CharField(label='Descrizione', max_length=45)
+
+class addMotPrestitoForm(forms.Form):
+	motivazione = forms.CharField(label='Motivazione', max_length=45)
+	descrizione = forms.CharField(label='Descrizione', max_length=45)
+
+class addTassoInteresseForm(forms.Form):
+	tipo = forms.CharField(label='Tipo', max_length=45)
+	percentuale  = forms.DecimalField(label='Descrizione', max_digits=5,decimal_places=2)
+	inizio = forms.DateField(label='Inizio')
+	fine = forms.DateField(label='Fine')
+
+class addTipoProvvedimentoForm(forms.Form):
+	tipo = forms.CharField(label='Tipo', max_length=45)
+	descrizione = forms.CharField(label='Descrizione', max_length=45) 
+
+class addPrestitoForm(forms.Form):
+	sede = forms.CharField(label='Sede', max_length=45)
+	descrizione = forms.CharField(label='Descrizione', max_length=45)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	tipo = forms.CharField(label='Tipo', max_length=20)
+	
